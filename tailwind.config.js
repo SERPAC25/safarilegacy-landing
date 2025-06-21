6ce0e77c8527129e8b610d1f77a71e713e1520a2
SERPAC25 <serpac.remote@gmail.com>
2025-06-21 21:11:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['"Playfair Display"', 'serif'],
+        body: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
 
