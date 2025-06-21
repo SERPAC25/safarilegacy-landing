f8f5d030c844be295fa37dbc1081a79e4eee6851
SERPAC25 <serpac.remote@gmail.com>
2025-06-21 21:47:13 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="flex items-center justify-center min-h-screen bg-black text-[#d4af37] px-4">
         <div className="text-center">
           <img
-            src="/logo-gold.png" // Update this if needed
+            src="/logo-gold.png"
             alt="SafariLegacy Logo"
             className="mx-auto w-64 md:w-80 mb-10"
           />
